d7d571456124d6a1da797cbec00d45d5dbc7ee0f
Rabin Rai <rabinrai44@gmail.com>
2018-09-04 13:54:11 -0400

    Added first part AddedNumber project

---
+++ AddNumber/AddNumber/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using AddNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AddNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(Test test)
+        {
+           if (ModelState.IsValid)
+            {
+                return RedirectToAction("ViewSum");
+            }           
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewSum(Test sum)
+        {
+            int getSum = sum.GetSum();
+            ViewBag.SumResult = getSum.ToString();
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Add(Test sum)
+        //{
+        //    int getSum = sum.GetSum();
+        //    ViewBag.SumResult = getSum.ToString();
+
+        //    return View("ViewSum");
+        //}
+    }
+}
---
+++ AddNumber/AddNumber/Models/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AddNumber.Models
+{
+    public class Test
+    {
+        [Range(1, 100, ErrorMessage = "The Number must be between 1-100.")]
+        public int Number1 { get; set; }
+        [Range(1, 100, ErrorMessage = "The Number must be between 1-100.")]
+        public int Number2 { get; set; }
+
+        public int GetSum(int num1, int num2)
+        {
+            num1 = Number1;
+            num2 = Number2;;
+            return num1 + num2;
+        }
+
+        public int GetSum()
+        {
+            return GetSum();
+        }
+    }
+}
